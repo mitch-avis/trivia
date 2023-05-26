fbe87f865cfac0ac5e394637c5a3219f3efc55c9
mitch-avis <mitchavis@gmail.com>
2023-05-26 13:33:08 -0600

    add categories and questions tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from flaskr import create_app
@@ -23,6 +24,30 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_categories(self):
+        res = self.client().get("/categories")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(len(data["categories"]))
+
+    def test_get_questions(self):
+        res = self.client().get("/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["totalQuestions"])
+        self.assertTrue(len(data["questions"]))
+        self.assertTrue(len(data["categories"]))
+
+    def test_get_questions_404_invalid_page(self):
+        res = self.client().get("/questions?page=1000")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
 
 
 # Make the tests conveniently executable
