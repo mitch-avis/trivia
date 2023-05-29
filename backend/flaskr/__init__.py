d65dc19a3a2fa0d2ec3da69f041cd8d7cf088440
mitch-avis <mitchavis@gmail.com>
2023-05-28 18:24:00 -0600

    add 500 error handler

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -227,4 +227,11 @@ def create_app(test_config=None):
             422,
         )
 
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return (
+            jsonify({"success": False, "error": 500, "message": "internal server error"}),
+            500,
+        )
+
     return app
